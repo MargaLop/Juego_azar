373952660bcc43cb5d1e3ce260f62b49d4989bd7
Marga <margalppalacios@gmail.com>
2022-04-25 13:42:46 +0200

    error

--- usuario.py
+++ usuario.py
@@ -10,14 +10,14 @@ class Usuario:
             self.num = int(input('¿cual es tu numero?:\n    '))
             sleep(0.3)
             
-            if self.num < 1 or self.num > 10 :
-                print("Este número no se encuentra entre el 0 al 10")
-                sleep(0.3)
+
 
         except Exception as error:
             error_string = str(error)
             print(error_string)
             
         
-        
+        if self.num < 1 or self.num > 10 :
+            print("Este número no se encuentra entre el 0 al 10")
+            sleep(0.3)
 
