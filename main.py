83b84fa6a97e5e74f3c3f9c610c37205d8ef3dc9
Marga <margalppalacios@gmail.com>
2022-04-25 12:18:53 +0200

    clases y objetos

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-import random
+# from juego import Juego
+from random import randint
 
-numero_azar = random.randint(1,10)
+class Usuario:
+    num = None
+    def introduce_numero(self):
+        self.num = int(input('¿cual es tu numero?:\n    '))
 
 
+class Juego:
+    num = None
+    usuario = None
 
-while(True):
-    numero_usuario= int(input('¿cual es tu numero?:'))
-
-    if numero_azar < numero_usuario:
-        print('más bajo')
-    elif numero_azar > numero_usuario:
-        print('más alto')
-    else:
+    def __init__(self):
+        self.num = randint(1,10)
+        self.usuario = Usuario()
+    
+    def juega(self):
+        while(self.usuario.num != self.num):
+            self.usuario.introduce_numero()
+            if self.num < self.usuario.num:
+                print('más bajo')
+            elif self.num > self.usuario.num:
+                print('más alto')
+            else:
+                self.victoria() 
+                
+        
+    def victoria(self):
         print('ACERTASTE')
-        break
+              
 
 
+
+
+if __name__ == '__main__':
+    juego = Juego()
+    juego.juega()
+
+    usuario = Usuario()
