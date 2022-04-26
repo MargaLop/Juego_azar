d6107325fa26fb30efa2ee44170ae1f491e5c5a7
Marga <margalppalacios@gmail.com>
2022-04-26 09:20:45 +0200

    Arreglar el error al introducir str

--- usuario.py
+++ usuario.py
@@ -1,23 +1,22 @@
-from time import sleep 
 import string
 class Usuario:
     num = None
     letras_abcdario = set(string.ascii_lowercase)
     def introduce_numero(self):
-        
-        try:
-            sleep(0.3)
-            self.num = int(input('¿cual es tu numero?:\n    '))
-            sleep(0.3)
+        while self.num == None:
+            try:
+                self.num = int(input('¿cual es tu numero?:\n    '))
+            except Exception as error:
+                error_string = str(error)
+                print(error_string)
+                continue;                
             
+            if self.num < 1:
+                print("Este número NO se encuentra entre el 1 al 10. Este número es menor de 1.")
+            
+            elif self.num > 10:
+                print("Este número NO se encuentra entre el 1 al 10. Este número es mayor de 10.")
 
 
-        except Exception as error:
-            error_string = str(error)
-            print(error_string)
-            
-        
-        if self.num < 1 or self.num > 10 :
-            print("Este número no se encuentra entre el 0 al 10")
-            sleep(0.3)
+
 
